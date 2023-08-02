58bfd2ca559f4ae397d5217879bfb03eb71d1e2f
dillytitan <80501424+dillytitan@users.noreply.github.com>
2023-08-02 01:55:13 -0700

    loadscreen tranistion

---
+++ components/loadScreen.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
+const LoadingScreen = () => (
+    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black">
+      <img src="/CyberCel.png" alt="Loading" />
+    </div>
+  );
+  
+  export default LoadingScreen;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,33 @@
 import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
+import { useEffect, useState } from 'react';
 import FirebaseProvider from "../lib/authContext";
 import "../lib/firebaseConfig/init";
 import "../styles/global.css"
+import LoadingScreen from "../components/loadScreen";
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <FirebaseProvider>
       <Layout>
-        <Component {...pageProps} />
+        {isLoading && <LoadingScreen />}
+        <div className={`transition-opacity ease-in-out duration-1000 ${isLoading ? 'opacity-0' : 'opacity-100'}`}>
+          <Component {...pageProps} />
+        </div>
       </Layout>
     </FirebaseProvider>
   );
 }
+
 export default MyApp;
