8a1d6834cdb64180874a15148a11683f88f22c51
dillytitan <80501424+dillytitan@users.noreply.github.com>
2023-08-02 01:00:03 -0700

    Update heroSlider.tsx

--- components/heroSlider.tsx
+++ components/heroSlider.tsx
@@ -3,36 +3,19 @@ import React from 'react';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/splide/dist/css/splide.min.css';
 
-
 type HeroImageCarouselProps = {
   images: string[];
 };
 
 const HeroImageCarousel: React.FC<HeroImageCarouselProps> = ({ images }) => (
-  <div className="hero-carousel-container">
-    <div className="flex-grow">
-    <Splide
-      options={{
-        type: 'loop',
-        direction: 'ttb',
-        autoplay: true,
-        interval: 3000,
-        pagination: true,
-        height: '21rem',
-        perPage: 1,
-
-        arrows: true, // Enable arrows
-      }}
-      
-    >
-      {images.map((image, index) => (
-        <SplideSlide key={index}>
-          <img src={image} alt={`Hero ${index}`} className="rounded-3xl" />
-        </SplideSlide>
-      ))}
-    </Splide>
-    </div>
-  </div>
+  <Splide options={{ type: 'loop', autoplay: true, interval: 3000, perPage: 1 }}>
+    {images.map((image, index) => (
+      <SplideSlide key={index}>
+        <img src={image} alt={`Hero ${index}`} className="rounded-3xl" />
+      </SplideSlide>
+    ))}
+  </Splide>
 );
 
 export default HeroImageCarousel;
+
